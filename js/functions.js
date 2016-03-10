f1d3f53bf3d2d167c7a0a824e85a91215f4f8bc1
Will Pleasant-Ryan <will.ryan@atomicobject.com>
2016-03-10 09:34:40 -0500

    bind

--- js/functions.js
+++ js/functions.js
@@ -1,10 +1,16 @@
 
 const add2 = function(x) { return x + 2 };
 
-const mult = function(x, y)  { return x + y };
+const mult = function(x, y) { return x * y };
 
 // bind, apply, etc.
-const mult2 = mult.bind(2);
+const mult2 = mult.bind(undefined, 2);
+
+module.exports = {
+  add2: add2,
+  mult: mult,
+  mult2: mult2
+};
 
 // composition
-const add2Mult2 = compose(add2, mult);
+//const add2Mult2 = compose(add2, mult);
