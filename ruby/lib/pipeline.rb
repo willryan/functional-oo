2d2188700f075298e17d22f3222a9c61661d1172
Will Pleasant-Ryan <will.ryan@atomicobject.com>
2016-03-12 12:44:26 -0500

    ruby examples are ready\!

--- ruby/lib/pipeline.rb
+++ ruby/lib/pipeline.rb
@@ -1,7 +1,28 @@
 require 'piece_pipe'
 require 'funkify'
 
+class ChangeRatio < PiecePipe::AssemblyStep
+  def receive(inputs)
+    install ratio_adjusted: (inputs[:celcius] * 9/5.0)
+  end
+end
+
+class AddOffset < PiecePipe::AssemblyStep
+  def receive(inputs)
+    install farenheit: (inputs[:ratio_adjusted] + 32.0)
+  end
+end
+
 class UsePiecePipe
+  def celsius_to_farenheit(temp)
+    PiecePipe::Pipeline.new.
+     source([{celcius: temp}]).
+     step(ChangeRatio).
+     step(AddOffset).
+     collect(:farenheit).
+     to_enum.
+     first
+  end
 end
 
 ##########
@@ -26,7 +47,7 @@ class FunkifyTemperature
     @funcs = UseFunkify.new
   end
 
-  def celsius_to_farenheight(temp)
+  def celsius_to_farenheit(temp)
     pass(temp) >=
       @funcs.mult(9/5.0) |
       @funcs.add(32.0)
--- ruby/spec/pipeline_spec.rb
+++ ruby/spec/pipeline_spec.rb
@@ -1,17 +1,19 @@
 require 'pipeline'
 
 describe 'piece pipe' do
+  let (:subject) { UsePiecePipe.new }
   it 'chains stuff' do
-    pending
-    raise 'todo'
+    expect(subject.celsius_to_farenheit(0.0)).to eq(32.0)
+    expect(subject.celsius_to_farenheit(-40.0)).to eq(-40.0)
+    expect(subject.celsius_to_farenheit(100.0)).to eq(212.0)
   end
 end
 
 describe 'funkify pipeline' do
   let (:subject) { FunkifyTemperature.new }
   it 'chains stuff' do
-    expect(subject.celsius_to_farenheight(0.0)).to eq(32.0)
-    expect(subject.celsius_to_farenheight(-40.0)).to eq(-40.0)
-    expect(subject.celsius_to_farenheight(100.0)).to eq(212.0)
+    expect(subject.celsius_to_farenheit(0.0)).to eq(32.0)
+    expect(subject.celsius_to_farenheit(-40.0)).to eq(-40.0)
+    expect(subject.celsius_to_farenheit(100.0)).to eq(212.0)
   end
 end
