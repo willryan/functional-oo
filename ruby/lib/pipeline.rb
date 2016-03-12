8a1c97bfeb638a82d50245e4fa8e6f41cc09ef91
Will Pleasant-Ryan <will.ryan@atomicobject.com>
2016-03-12 09:17:06 -0500

    funkify composition

--- ruby/lib/pipeline.rb
+++ ruby/lib/pipeline.rb
@@ -1,9 +1,7 @@
 require 'piece_pipe'
+require 'funkify'
 
 class UsePiecePipe
-end
-
-class UseFunkify
 end
 
 describe 'piece pipe' do
@@ -13,9 +11,41 @@ describe 'piece pipe' do
   end
 end
 
+##########
+
+class UseFunkify
+  include Funkify
+
+  auto_curry
+  def add(x, y)
+    x + y
+  end
+
+  def mult(x, y)
+    x * y
+  end
+end
+
+class FunkifyTemperature
+  include Funkify
+
+  def initialize
+    @funcs = UseFunkify.new
+  end
+
+  def celsius_to_farenheight(temp)
+    pass(temp) >=
+      @funcs.mult(9/5.0) |
+      @funcs.add(32.0)
+  end
+
+end
+
 describe 'funkify pipeline' do
+  let (:subject) { FunkifyTemperature.new }
   it 'chains stuff' do
-    pending
-    raise 'todo'
+    expect(subject.celsius_to_farenheight(0.0)).to eq(32.0)
+    expect(subject.celsius_to_farenheight(-40.0)).to eq(-40.0)
+    expect(subject.celsius_to_farenheight(100.0)).to eq(212.0)
   end
 end
