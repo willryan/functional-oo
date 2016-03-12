a311c03625c548226f6d454c4275cf3025343015
Will Pleasant-Ryan <will.ryan@atomicobject.com>
2016-03-12 08:57:52 -0500

    test framework ready for csharp examples

--- csharp/FunctionalOO/StandardDependencyInjection.cs
+++ csharp/FunctionalOO/StandardDependencyInjection.cs
@@ -1,6 +1,8 @@
 using System.Net;
 using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
+using Moq;
+using NUnit.Framework;
 
 namespace FunctionalOO
 {
@@ -21,6 +23,19 @@ namespace FunctionalOO
         public string UseDependency(string url)
         {
             return _dependency.GoGetSomeValue(IPAddress.Loopback, url).Result;
+        }
+    }
+
+    [TestFixture]
+    public class MockDependency
+    {
+        [Test]
+        public void Test_UseDependency()
+        {
+            var dep = new Mock<IDepdencency>();
+            var subject = new StandardDependencyInjection(dep.Object);
+            dep.Setup(d => d.GoGetSomeValue(IPAddress.Loopback, "/foo/bar")).ReturnsAsync("it worked");
+            Assert.That(subject.UseDependency("/foo/bar"), Is.EqualTo("it worked"));
         }
     }
 }
