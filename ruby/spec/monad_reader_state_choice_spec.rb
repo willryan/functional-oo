17300130441843f2b93fdc8c5b8835bde475ef3c
Will Pleasant-Ryan <will.ryan@atomicobject.com>
2016-03-13 20:13:55 -0400

    shorter lines for ruby spec

--- ruby/spec/monad_reader_state_choice_spec.rb
+++ ruby/spec/monad_reader_state_choice_spec.rb
@@ -2,11 +2,14 @@ require 'monad_reader_state_choice'
 
 describe 'reader state choice monad' do
   it 'handles correct cases' do
-    expect(ReaderStateChoiceExample.state_func(:english, [5,14,8]).success_value).to eq("one, five, two")
-    expect(ReaderStateChoiceExample.state_func(:spanish, [5,14,8]).success_value).to eq("uno, cinco, dos")
+    val = ReaderStateChoiceExample.state_func(:english, [5,14,8])
+    expect(val.success_value).to eq("one, five, two")
+    val = ReaderStateChoiceExample.state_func(:spanish, [5,14,8])
+    expect(val.success_value).to eq("uno, cinco, dos")
   end
 
   it 'stops early' do
-    expect(ReaderStateChoiceExample.state_func(:english, [5,8,8]).failure_value).to eq("too big")
+    val = ReaderStateChoiceExample.state_func(:english, [5,8,8])
+    expect(val.failure_value).to eq("too big")
   end
 end
